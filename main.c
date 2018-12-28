1a5fbc7bfe11aea2d750b6d0408ebf572fc33518
andreyuhai <burak.kaymakci@bil.omu.edu.tr>
2018-12-28 10:51:39 +0100

    client-server codes written from scratch

--- main.c
+++ main.c
@@ -10,12 +10,13 @@
 int *seats = NULL;
 
 // Semaphore for the so-called "line"
-sem_t *line = NULL;
-sem_t s_server;
+sem_t *clients = NULL;
+sem_t *servers = NULL;
+sem_t s_client;
 
 // Seat number
 int seatNum;
-int customerInLine;
+int seatReserved = 0;
 
 // Creating a struct to pass to client threads with the customer ID and number of seats.
 struct thread_arguments{
@@ -32,66 +33,57 @@ void sleepRandom() {
     // Get a random sleep time between 50 and 200 (inclusive)
     unsigned int sleepTime = 50 + (rand() % 151);
 
-    printf("Sleeping %d milliseconds", sleepTime);
-
     // Then sleep...zZz
     usleep(sleepTime * 1000);
 
 }
 
-void client(void *args) {
+void *client(void *args) {
 
     struct thread_arguments *arguments = args;
 
-    sleepRandom();
-
+    sem_wait(&s_client);
 
     while(1) {
 
-        sem_wait(&line[arguments->customerID]);
+        sem_wait(&clients[arguments->customerID]);
 
-        if(customerInLine != arguments->customerID) {
 
-            printf("Exiting thread");
+        if(!seatReserved) {
+
+            seatNum = generateRandSeatNum(arguments->numSeats);
+            printf("Generated seat num = %d by client %d\n", seatNum, arguments->customerID);
+            sem_post(&servers[arguments->customerID]);
+
+        } else {
+            printf("Exiting thread\n");
+            seatReserved = 0;
+            sem_post(&s_client);
             pthread_exit(NULL);
         }
-
-        seatNum = generateRandSeatNum(arguments->numSeats);
-
-        sem_post(&s_server);
-
     }
 
 }
 
-void server(void *args) {
+void *server(void *args) {
 
     struct thread_arguments *arguments = args;
 
-    sem_post(&line[arguments->customerID]);
-
-    sem_wait(&s_server);
-
-
     while(1) {
-        // If the seat is available.
-        if(!seatNum) {
+        sem_wait(&servers[arguments->customerID]);
 
+        if(!seats[seatNum]) {
+            printf("Seat number %d reserved for customer %d\n", seatNum, arguments->customerID);
+            seatReserved = 1;
             seats[seatNum] = arguments->customerID;
-            printf("Seat number : %d is reserved for client : %d\n", seatNum+1, arguments->customerID+1);
-            customerInLine = -1;
-            sem_post(&line[customerInLine]);
-            sem_post(&s_server);
+            sem_post(&clients[arguments->customerID]);
             pthread_exit(NULL);
-
-        } else { // If the seat is not available
-
-            sem_post(&line[customerInLine]);
-            printf("Seat was not available");
-
+        }else {
+            printf("Seat was not available\n");
+            sem_post(&clients[arguments->customerID]);
         }
-
     }
+
 }
 
 int main(int argc, char *argv[]) {
@@ -106,24 +98,51 @@ int main(int argc, char *argv[]) {
     seats = calloc(numSeats, sizeof(int));
 
     if(seats == NULL) {
-        printf("There was an error callocing for seats");
+        printf("There was an error callocing for seats\n");
         exit(-1);
     }
 
     // Setting each seat (char actually) to 0 which means available.
     memset(seats, 0, numSeats);
 
-    // Create as many semaphores as there are clients in the line.
-    line = calloc(numSeats, sizeof(sem_t));
+    clients = calloc(numSeats, sizeof(sem_t));
+    servers = calloc(numSeats, sizeof(sem_t));
 
-    for(int i = 0; i < numSeats; i++) {
-        sem_init(&line[i], 0, 0); // Initializing semaphores for the line.
+    for (int i = 0; i < numSeats; i++) {
+        sem_init(&clients[i], 0, 1);
+        sem_init(&servers[i], 0, 0);
+    }
+    sem_init(&s_client, 0, 1);
+
+    pthread_t *clientID = calloc(numSeats, sizeof(pthread_t));
+    pthread_t *serverID = calloc(numSeats, sizeof(pthread_t));
+
+    struct thread_arguments *t_args = calloc(numSeats, sizeof(struct thread_arguments));
+
+    for (int i = 0; i < numSeats; i++) {
+
+        t_args[i].customerID = i;
+        t_args[i].numSeats = numSeats;
+
     }
 
-    // Initializing semaphore for the servers.
-    sem_init(&s_server, 0, 0);
-    
+    for (int i = 0; i < numSeats ; i++) {
+        pthread_create(&clientID[i], NULL, client, &t_args[i]);
+        pthread_create(&serverID[i], NULL, server, &t_args[i]);
+    }
 
+    for (int i = 0; i < numSeats ; i++) {
+        pthread_join(clientID[i], NULL);
+        pthread_join(serverID[i], NULL);
+    }
+
+
+    puts("---------------");
+    for (int i = 0; i < numSeats; i++) {
+
+        printf("Seat num %d reserved for customer num %d\n", i+1, seats[i]+1);
+
+    }
 
     // Freeing seats at the end of the program to avoid memory leak.
     free(seats);
